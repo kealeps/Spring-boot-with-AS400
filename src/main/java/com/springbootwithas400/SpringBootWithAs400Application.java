c3629fede09dc40bd04744df642f2a8cd05746d7
Kevin Perez <mr.kv17.kp@gmail.com>
2021-01-22 08:53:53 -0400

    Primer commit, Conexion a base de datos DB2 usando jpa con exito

---
+++ src/main/java/com/springbootwithas400/SpringBootWithAs400Application.java
@@ -0,0 +1,13 @@
+package com.springbootwithas400;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithAs400Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithAs400Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootwithas400/SpringBootWithAs400ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootwithas400;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithAs400ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
